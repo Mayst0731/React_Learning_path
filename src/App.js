bf78e171616296aa1c23743efd9ed0eb97eb87df
apple <xiaomei6@illinois.edu>
2020-06-20 17:51:03 -0500

    change state info

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  //  create a constructor function first, constructor should be lowercase
+  constructor(){
+    super();
+
+    this.state = {
+      monsters : [{
+        name : 'Frankenstein',
+        id : 'asc1'
+      },
+      {
+        name : 'Zombie',
+        id : 'asc2'
+      },
+      { name :'Dracula',
+        id : 'asc3'
+      }]
+    };
+  };
   render(){
     return (
+      // className can avoid repeatition because class can also be used in js class
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Xiaomei
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+          <h1 key = {monster.id}>{monster.name}</h1>
+          ))
+        }
       </div>
     );
   }
